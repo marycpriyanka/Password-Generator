a673302fe7502244768783584a9d18b858e7eb3b
Priyanka Mary Chrstine <marycpriyanka@gmail.com>
2022-01-06 10:25:51 -0800

    Update all character types

--- assets/script/script.js
+++ assets/script/script.js
@@ -66,10 +66,10 @@ function validateLength(length)
 function getPasswordCharacterList()
 {
   //Defines the character types.
-  const lowerCaseChar = ["a", "b", "c"];
-  const upperCaseChar = ["A", "B", "C"];
-  const numericChar = ["0", "1", "2"];
-  const specialChar = [" ", "!", "\"", "#"];
+  const lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  const upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "W", "Y", "Z"];
+  const numericChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const specialChar = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", ".", "-", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
   // Initializes the password character list to empty.
   let passwordCharList = [];
